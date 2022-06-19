12096f5d91e271b74a3b9e24f3761af682333e1e
m r <trash89@laposte.net>
2022-06-19 21:40:18 +0200

    start with react

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span></span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App";
+import "normalize.css";
+import "./index.css";
+
+import { themeOptions } from "./components/MUITheme";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+
+const theme = createTheme(themeOptions);
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/",
+});
+const clientApollo = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ApolloProvider client={clientApollo}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </ApolloProvider>
+    </Provider>
+  </React.StrictMode>
+);
